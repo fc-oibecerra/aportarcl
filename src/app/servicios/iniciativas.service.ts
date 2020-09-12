4c97ab2047308b6e3cca503c0442ddf95fc14f44
fc-oibecerra <obecerra@conservador.cl>
2020-09-12 01:52:45 -0300

    update

--- src/app/servicios/iniciativas.service.ts
+++ src/app/servicios/iniciativas.service.ts
@@ -6,60 +6,11 @@ export class IniciativasService {
 
   private eventos:Evento[] = [
     {
-      nombre: "Trauma Ocular",
-      bio: "Evento en apoyo a coordinadora de victimas de trauma ocular.",
-      img: "assets/img/trauma-ocular.jpg",
-      aparicion: "2020-08-14",
-      casa:"organización social"
-    },
-    {
-      nombre: "Olla Común Bajos de Mena",
-      bio: "Actualmente reunimos fondos para ir en apoyo de ollas comunes en santiago y Viña del Mar. ",
-      img: "assets/img/olla-digna.jpg",
-      aparicion: "2020-09-14",
-      casa:"olla común"
-    },
-    {
-      nombre: "Red de apoyo al migrante",
-      bio: "Evento en apoyo a coordinadora de victimas de trauma ocular.",
-      img: "assets/img/trauma-ocular.jpg",
-      aparicion: "2020-08-14",
-      casa:"organización social"
-    },
-    {
-      nombre: "Cantata por la vida",
-      bio: "Actualmente reunimos fondos para ir en apoyo de ollas comunes en santiago y Viña del Mar. ",
-      img: "assets/img/olla-digna.jpg",
-      aparicion: "2020-09-14",
-      casa:"organización cultural"
-    },
-    {
-      nombre: "Junta de alimentos",
-      bio: "Evento en apoyo a coordinadora de victimas de trauma ocular.",
-      img: "assets/img/trauma-ocular.jpg",
-      aparicion: "2020-08-14",
-      casa:"obra social"
-    },
-    {
-      nombre: "Apoyo Hogar niños Puente Alto",
-      bio: "Actualmente reunimos fondos para ir en apoyo de ollas comunes en santiago y Viña del Mar. ",
-      img: "assets/img/olla-digna.jpg",
-      aparicion: "2020-09-14",
-      casa:"organización social"
-    },
-    {
-      nombre: "Trauma Ocular",
-      bio: "Evento en apoyo a coordinadora de victimas de trauma ocular.",
-      img: "assets/img/trauma-ocular.jpg",
-      aparicion: "2020-08-14",
-      casa:"olla común"
-    },
-    {
-      nombre: "Olla Digna",
-      bio: "Actualmente reunimos fondos para ir en apoyo de ollas comunes en santiago y Viña del Mar. ",
-      img: "assets/img/olla-digna.jpg",
-      aparicion: "2020-09-14",
-      casa:"olla común"
+      nombre: "Talleres de monitores Constitucionales",
+      bio: "Taller abierto para formación de ciudadanos que a su vez puedan formar a otros en los aspectos principales del proceso constituyente. Inscribete aqui <a href="">Formulario de inscripción</a>",
+      img: "assets/iniciativas/mellado.jpg",
+      aparicion: "2020-10-05",
+      casa:"taller"
     }
   ];
 
